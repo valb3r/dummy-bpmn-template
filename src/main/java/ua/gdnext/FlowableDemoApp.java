fa9632762e0898a53983a6fcab9022a7af39f358
valb3r <valb3r.ua@gmail.com>
2024-10-10 09:48:25 +0300

    Initial project structure

---
+++ src/main/java/ua/gdnext/FlowableDemoApp.java
@@ -0,0 +1,12 @@
+package ua.gdnext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableDemoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableDemoApp.class, args);
+    }
+}
